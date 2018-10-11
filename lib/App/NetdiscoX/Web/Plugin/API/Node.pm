53ec36da2794c7bff3c50d5792421cde8f164a8b
Ryan Gasik <pyro3d@gmail.com>
2018-10-11 13:46:49 -0700

    Cleanup Node

--- lib/App/NetdiscoX/Web/Plugin/API/Node.pm
+++ lib/App/NetdiscoX/Web/Plugin/API/Node.pm
@@ -32,6 +32,7 @@ sub api_array_json {
 get '/api/node/search' => sub {
     my $para = params;
     my $search = {};
+    # Generate a hashref of search terms. 
     foreach my $param (keys %{$para}) {
         if ($param ne 'return_url') {
             $search->{$param} = $para->{$param};
@@ -47,15 +48,18 @@ get '/api/node/search' => sub {
 get '/api/node/:node/:method' => sub {
     my $node = params->{node};
     my $method = params->{method};
-    my @nodeip;
-    if (!($node =~ m/[0-9a-z:]{17}/)){
+    # Make sure $node is actually a mac address in the proper format.
+    if (!($node =~ m/([0-9a-f]{2}:){5}([0-9a-f]{2})/)){
         status 400;
-        return ({ error => "Not a MAC Address. Address must follow aa:bb:cc:dd:ee:ff" });
+        return ({ error => "Not a MAC Address. Address must follow the format aa:bb:cc:dd:ee:ff." });
     }
     try {
-        @nodeip = schema('netdisco')->resultset('Node')->search({ mac => $node});
-        my $node = $nodeip[0]->$method;
-        # ResultSets need to be converted to an array of hashes before being returned.  
+        my @nodesearch = schema('netdisco')->resultset('Node')->search({ mac => $node});
+        # Searching by mac there should be only one result
+        my $node = $nodesearch[0]->$method;
+
+        # ResultSets need to be converted to an array of hashes before being returned.
+        # Dancer's JSON serializer doesn't like the objects
         if (ref($node) =~ m/ResultSet/) {
             my @nodes = $node->all;
             return api_array_json(\@nodes);
@@ -64,16 +68,14 @@ get '/api/node/:node/:method' => sub {
             my $nodes = $node;
             return $nodes->{_column_data};
         }
-        my (@nodes) = $nodeip[0]->$method;
-        return "{}";
     } catch {
         my ($exception) = @_;
         print Dumper($exception);
         if ($exception =~ m/Can\'t call method "$method" on an undefined value/) {
             status 404;
-            return ({ error => "MAC Address not found"});
+            return ({ error => "MAC Address not found."});
         }
         status 400;
-        return ({ error => "Invalid collection!" });
+        return ({ error => "Invalid collection $method." });
     };
 };
